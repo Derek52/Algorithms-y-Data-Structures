58667e2adbe416ddfb6d706fb7205730b736c58e
Derek Briggs <contact@derekbriggs.xyz>
2022-11-16 18:23:13 -0500

    Add extra functions to Stack

--- Kotlin/Data Structures/Stack.kt
+++ Kotlin/Data Structures/Stack.kt
@@ -1,26 +1,41 @@
-class Stack(val size : Int) {
+class Stack(val maxSize : Int) {
 	val innerArray : IntArray
 
 	var top = 0
+	var currentSize : Int = 0
 
 	init {
-		innerArray = IntArray(size)
+		innerArray = IntArray(maxSize)
 	}
 
 	fun put(item : Int) {
 		innerArray[top] = item
 		top++
+		currentSize++
 	}
 
 	fun pop() : Int {
 		val item = innerArray[top-1]
 		innerArray[top-1] = 0
 		top--
+		currentSize--
 		return item
 	}
 
 	fun peek() : Int {
 		return innerArray[top-1]
+	}
+
+	fun isFull() : Boolean {
+		return (currentSize == maxSize - 1)
+	}
+
+	fun isEmpty() : Boolean {
+		return currentSize == 0
+	}
+
+	fun size() : Int {
+		return currentSize
 	}
 }
 
@@ -32,6 +47,7 @@ fun main() {
 	stack.put(49)
 	stack.put(64)
 	println(stack.innerArray.joinToString(", "))
+	println("Stack has ${stack.currentSize} elements")
 	stack.pop()
 	stack.pop()
 	stack.pop()
